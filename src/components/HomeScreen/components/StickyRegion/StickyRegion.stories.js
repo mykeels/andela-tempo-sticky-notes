a4c7b47f6c17538dbfecfa81bcb28101ec79bac1
mykeels <mykehell123@gmail.com>
2022-10-03 10:46:10 -0400

    feat: add first component

---
+++ src/components/HomeScreen/components/StickyRegion/StickyRegion.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { StickyRegion } from "./index";
+
+export default {
+  title: "components/StickyRegion",
+  component: StickyRegion
+};
+
+export const Index = () => <StickyRegion />;
---
+++ src/components/HomeScreen/components/StickyRegion/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ * @param {object} props
+ * @param {any} props.children
+ * @returns {JSX.Element}
+ */
+export const StickyRegion = ({ children }) => {
+  return <div className="bg-gray-400"></div>;
+};
