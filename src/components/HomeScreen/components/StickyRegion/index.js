6776cbd2c01e5f66340acad60e2f3813d8696752
mykeels <mykehell123@gmail.com>
2022-10-03 11:04:05 -0400

    feat: style StickyRegion

--- src/components/HomeScreen/components/StickyRegion/index.js
+++ src/components/HomeScreen/components/StickyRegion/index.js
@@ -1,3 +1,5 @@
+import "./StickyRegion.css";
+
 import React from "react";
 
 /**
@@ -6,5 +8,5 @@ import React from "react";
  * @returns {JSX.Element}
  */
 export const StickyRegion = ({ children }) => {
-  return <div className="bg-gray-200">{children}</div>;
+  return <div className="bg-gray-200 block sticky-region">{children}</div>;
 };
