9bd8532c79e48e3e8013f7176f40b5d68003c8d5
mykeels <mykehell123@gmail.com>
2023-03-19 14:13:36 -0400

    fix: baseUrl

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const root = process.env.PUBLIC_URL || "";
   return (
     <Html lang="en">
       <Head>
-        <base href={process.env.PUBLIC_URL || "/"} />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <base href={root} />
+        <link rel="shortcut icon" href={`${root}/favicon.svg`} />
         <meta
           name="description"
           content="Simple sticky notes app built with Next.js, React, TypeScript, Tailwind CSS, and React Query"
